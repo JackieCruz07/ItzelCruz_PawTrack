9be7a0a1501810d932678b8db4b2cb8f26eafc2c
Itzel Cruz <149315684+JackieCruz07@users.noreply.github.com>
2024-08-25 18:19:19 -0600

    codigo terminado

--- 02-repaso.js
+++ 02-repaso.js
@@ -11,3 +11,68 @@ const datosArreglo = () => {
     })
 };
 datosArreglo();
+
+// for of
+let A = [[3, 4, 5, 6], [7, 8, 9]];
+
+const datosArreglo = () => {
+    for (let datos of A) {
+        for (let i = 0; i < datos.length; i++) {
+            console.log(`${i} = ${datos[i]}`);
+        }
+    }
+};
+datosArreglo();
+
+//for in
+let A = [[3, 4, 5, 6], [7, 8, 9]];
+
+const datosArreglo = () => {
+    for (let datos of A) {
+        let datos = A[index]
+        for (let i in datos) {
+            console.log(`${i} = ${datos[i]}`);
+        }
+    }
+};
+datosArreglo();
+
+//for each
+let A = [[3, 4, 5, 6], [7, 8, 9]];
+
+const datosArreglo = () => {
+    A.forEach((datos) => {
+        datos.forEach((valores, i) => {
+            console.log(`${i} = ${valores}`);
+        })
+    })
+};
+datosArreglo();
+
+//function flecha
+let A = [[3, 4, 5, 6], [7, 8, 9]];
+
+const datosArreglo = () => {
+    A.map((datos) =>
+        datos.map((valores, i) => {
+            console.log(`${i} = ${valores}`);
+        })
+    )
+};
+datosArreglo();
+
+//while
+let A = [[3, 4, 5, 6], [7, 8, 9]];
+
+const datosArreglo = () => {
+    let i = 0
+    while (i < A.length) {
+        let j = 0
+        while (j < A[i].length) {
+            console.log(`${j} = ${A[i][j]}`)   
+            j++         
+        }
+        j++
+    }
+}
+datosArreglo()
