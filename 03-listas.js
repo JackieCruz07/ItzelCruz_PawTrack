ed5105476e920c535bc726854de5317f1dfeecb1
Itzel Cruz <149315684+JackieCruz07@users.noreply.github.com>
2024-08-27 09:52:28 -0600

    primer codigo

---
+++ 03-listas.js
@@ -0,0 +1,10 @@
+//crear una lista con diferentes tipos de datos
+let datos={nombre:"jackie", apellido:"Cruz", edad:18, estudiante:true}
+let arreglos=["Jackie", 18, true]
+
+const info=()=>{
+    for(let arr in arreglo){
+        console.log(arr)
+    }
+}
+info()
