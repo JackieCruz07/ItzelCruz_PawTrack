85991e7d7724e1221d9f6df4d5bbd11726098666
Itzel Yajaira Cruz Hernández <itzelyajairacruzhernandez7@gmail.com>
2024-10-25 19:16:29 -0600

    rutas

--- primer-react/src/App.js
+++ primer-react/src/App.js
@@ -1,31 +1,19 @@
-import "./App.css";
-import Otro from "./otro";
-import FormularioCss from "./formulariocss";
-import Formularioboot from "./components/formularioboot";
+import './App.css';
+/* import Otro from "./Otro";
+import Formulariocss from "./Formulariocss";
+import Formularioboot from "./components/Formularioboot"; */
+import { BrowserRouter as Router } from "react-router-dom";
+import Menu from "./components/inicio/Menu";
+import Rutas from "./routes/Rutas"
 
 function App() {
   return (
-    <>
-    <div className="row">
-    <div className="col-4">
-      <h1>Columnas a la izquierda</h1>
-    </div>
-    <div className="col-4">
-      <h1>Centro</h1>
-    </div>
-    <div className="col-4">
-      <h1>Derecha</h1>
-    </div>
-    </div>
-    <div className="App" >
-      <h1>Hi</h1>
-      <Otro />
-      <FormularioCss/>
-      <div className="container-fluid p-3">
-       <Formularioboot/> 
+<Router>
+      <div className="container-fluid">
+        <Menu />
+        <Rutas/>
       </div>
-    </div>
-    </>
+</Router>
   );
 }
 
---
+++ primer-react/src/components/calificaciones/ViewCalificaciones.js
@@ -0,0 +1,7 @@
+import React from 'react'
+function ViewCalificaciones() {
+  return (
+    <div>ViewCalificaciones</div>
+  )
+}
+export default ViewCalificaciones
